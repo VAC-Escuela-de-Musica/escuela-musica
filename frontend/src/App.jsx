ab48db6529a84816e75501e772c1b56481f4fca4
nebcore <victor.aguilera2102@alumnos.ubiobio.cl>
2025-07-05 01:47:41 -0400

    acceso a login en front creado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
 import React from "react";
-import Hero from "./components/Hero";
-import CarouselClases from "./components/Carrusel";
-import Profesores from "./components/Profesores";
-import Galeria from "./components/Galeria";
-import Testimonios from "./components/Testimonios";
-import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
-      <CarouselClases />
-      <Profesores />
-      <Galeria />
-      <Testimonios />
-      <Footer />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Navbar />
+            <HomePage />
+          </>
+        } />
+        <Route path="/login" element={<LoginPage />} />
+        {/* agregar más rutas */}
+      </Routes>
     </div>
   );
 }
